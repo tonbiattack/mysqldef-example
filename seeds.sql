716317770db3f178fe350cef64a47db0242ed797
tonbiattack <40497724+tonbiattack@users.noreply.github.com>
2026-03-28 23:25:21 +0900

    seeds.sqlにUTF-8MB4文字セットの設定を追加

--- seeds.sql
+++ seeds.sql
@@ -2,6 +2,7 @@
 -- 何度実行しても同じ結果になるよう ON DUPLICATE KEY UPDATE で冪等に書く
 -- ※ このファイルから行を削除してもDBのレコードは自動削除されない
 
+set names utf8mb4;
 -- ステータス区分値マスタ
 INSERT INTO status_types (code, name) VALUES
     (1, '受付'),
